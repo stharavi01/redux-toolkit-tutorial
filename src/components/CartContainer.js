96d94ba221e7c50f0e73618fc29bdefd989a3a02
Rabin Shrestha <sanayashe@gmail.com>
2023-10-31 15:50:51 +0545

    fetch data

---
+++ src/components/CartContainer.js
@@ -0,0 +1,31 @@
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
+
+const CartContainer = () => {
+	const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+	if (amount < 1) {
+		return (
+			<section className="cart">
+				<header>
+					<h2>your bag</h2>
+					<h4 className="empty-cart">is currently empty</h4>;
+				</header>
+			</section>
+		);
+	}
+
+	return (
+		<section className="cart">
+			<header>
+				<h2>Your bag</h2>
+				<div>
+					{cartItems.map((item) => {
+						return <CartItem key={item.id} {...item} />;
+					})}
+				</div>
+			</header>
+		</section>
+	);
+};
+export default CartContainer;
