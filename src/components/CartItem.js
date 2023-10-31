9393d3f74907af749aa198ba4aad1ccf7ac2b76d
Rabin Shrestha <sanayashe@gmail.com>
2023-10-31 16:46:59 +0545

    rendering cartItems

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,4 +1,24 @@
-const CartItem = () => {
-	return <div>CartItem</div>;
+import { ChevronUp, ChevronDown } from "../icons";
+
+const CartItem = ({ id, img, title, price, amount }) => {
+	return (
+		<article className="cart-item">
+			<img src={img} alt={title} />
+			<div>
+				<h4>{title}</h4>
+				<h4 className="item-price">${price}</h4>
+				<button className="remove-btn">Remove</button>
+			</div>
+			<div>
+				<button className="amount-btn">
+					<ChevronUp />
+				</button>
+				<p className="amount">{amount}</p>
+				<button className="amount-btn">
+					<ChevronUp />
+				</button>
+			</div>
+		</article>
+	);
 };
 export default CartItem;
