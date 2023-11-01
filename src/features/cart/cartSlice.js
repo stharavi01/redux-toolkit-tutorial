f02d2e24f29d6fa936bde1e2d82c331a61250f24
Rabin Shrestha <sanayashe@gmail.com>
2023-11-01 15:15:02 +0545

    final commit

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,5 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import cartItems from "../../cartItems";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
 
 const initialState = {
 	cartItems: cartItems,
@@ -7,6 +9,20 @@ const initialState = {
 	total: 0,
 	isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk("cart/getCartItems", async () => {
+	try {
+		const response = await fetch(url);
+		if (!response.ok) {
+			throw new Error("Network response was not ok");
+		}
+		const data = await response.json(); // Return the JSON data
+		return data;
+	} catch (error) {
+		console.error("Error fetching cart items:", error);
+		throw error;
+	}
+});
 
 const cartSlice = createSlice({
 	name: "cart",
@@ -37,6 +53,20 @@ const cartSlice = createSlice({
 			state.amount = amount;
 			state.total = total;
 		},
+	},
+
+	extraReducers: (builder) => {
+		builder
+			.addCase(getCartItems.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(getCartItems.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.cartItems = action.payload;
+			})
+			.addCase(getCartItems.rejected, (state, action) => {
+				state.isLoading = false;
+			});
 	},
 });
 
