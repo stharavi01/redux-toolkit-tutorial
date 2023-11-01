fbbe589cb643cef023873250f9d98da93d6130f6
Rabin Shrestha <sanayashe@gmail.com>
2023-11-01 11:40:26 +0545

    total

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { useEffect } from "react";
+import { calculateTotals } from "./features/cart/cartSlice";
 function App() {
+	const { cartItems } = useSelector((store) => store.cart);
+	const dipatch = useDispatch();
+
+	useEffect(() => {
+		dispatchEvent(calculateTotals());
+	}, [cartItems]);
 	return (
 		<main>
 			<Navbar />
